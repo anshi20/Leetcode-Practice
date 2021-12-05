90de20c5950a0a5791913418ba724eebb179f186
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-12-05 18:19:43 +0530

    Create 4.cpp

---
+++ Pattern Printing/4.cpp
@@ -0,0 +1,25 @@
+/*
+Enter n : 5
+
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(i=n;i>0;i--){
+        for(j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
