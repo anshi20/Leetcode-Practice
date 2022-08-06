69e50c5948f3e72e473521667f8d56de1bf8efcb
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-08-06 12:01:34 +0530

    Update MajorityElement.cpp

--- Hashmap/MajorityElement.cpp
+++ Hashmap/MajorityElement.cpp
@@ -3,6 +3,8 @@ Given an array nums of size n, return the majority element.
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 */
 
+
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -24,5 +26,26 @@ public:
           }
       }
       return max;
+    }
+};
+
+
+//Using map
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       unordered_map<int,int> m;
+       for(auto i : nums)
+       {
+           m[i]++;
+           
+       }
+       
+       for(auto i : nums)
+       {
+           if(m[i]>nums.size()/2)
+               return i;
+       }
+       return -1;
     }
 };
