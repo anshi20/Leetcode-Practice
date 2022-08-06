178e787ca374439d95cd56244ca8fbe7c5028c82
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-08-06 11:31:04 +0530

    Update ContainsDuplicate.cpp

--- Hashmap/ContainsDuplicate.cpp
+++ Hashmap/ContainsDuplicate.cpp
@@ -6,7 +6,7 @@ Output: true
 
 */
 
-
+//Using map
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -22,5 +22,23 @@ public:
                 return true;
         }
         return false;
+    }
+};
+
+
+//other approach
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+      int i, x;
+      for(i=nums.size()-1;i>0;i--)
+      {
+          x=(nums[i]^nums[i-1]);
+          if(x==0)
+              return true;
+      }
+        return false;
+   
     }
 };
